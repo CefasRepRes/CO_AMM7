f0d22367dabe726d740e3be27ee0f4914449e6a4
jenniferagraham <jennifer.graham@cefas.co.uk>
2024-02-21 11:05:58 +0000

    Links to new boundary forcing

--- EXP4.0.4-parcels/setup-paths.sh
+++ EXP4.0.4-parcels/setup-paths.sh
@@ -1,15 +1,16 @@
 # set input paths to those used here: https://github.com/CefasRepRes/CO_AMM7
 
-INPUT=/gpfs/data/cefas-shared/NEMO/jmmp/AMM7/
+INPUT=/gpfs/data/cefas-shared/NEMO/jmmp/AMM7
+BDY_INPUT=/gpfs/data/cefas-shared/AMM7_reanalysis_V5/INPUTS
 
-ln -s ${INPUT}/inputs/BDY .
-ln -s ${INPUT}/inputs/BDY_SKAG .
+ln -s ${BDY_INPUT}/LBC/GLOSEA BDY
+ln -s ${BDY_INPUT}/LBC/BAL BDY_SKAG
+ln -s ${BDY_INPUT}/NOWMAPS_rivers RIV
+ln -s ${BDY_INPUT}/ERA5 SBC
 ln -s ${INPUT}/grid/coordinates.bdy.nc .
 ln -s ${INPUT}/grid/coordinates.skagbdy.nc .
 ln -s ${INPUT}/grid/domain_cfg_co9amm7_MEsL51r10-07.nc .
 ln -s ${INPUT}/inputs/IC/initcd_y2005m01.nc .
-ln -s ${INPUT}/inputs/RIV . 
-ln -s ${INPUT}/inputs/SBC .
 ln -s ${INPUT}/inputs/TIDE .
 
 mkdir RESTARTS

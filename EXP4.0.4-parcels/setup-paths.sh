402f1d0a0c1b1dda1e9f5582ca6242afd44f2a65
jenniferagraham <jennifer.graham@cefas.co.uk>
2024-02-22 14:59:00 +0000

    Update for new ERA5 forcing

--- EXP4.0.4-parcels/setup-paths.sh
+++ EXP4.0.4-parcels/setup-paths.sh
@@ -6,8 +6,7 @@ BDY_INPUT=/gpfs/data/cefas-shared/AMM7_reanalysis_V5/INPUTS
 ln -s ${BDY_INPUT}/LBC/GLOSEA BDY
 ln -s ${BDY_INPUT}/LBC/BAL BDY_SKAG
 ln -s ${BDY_INPUT}/NOWMAPS_rivers_1991_2024 RIV
-#ln -s ${BDY_INPUT}/ERA5 SBC
-ln -s ${INPUT}/inputs/SBC .
+ln -s ${BDY_INPUT}/SBC .
 ln -s ${INPUT}/grid/coordinates.bdy.nc .
 ln -s ${INPUT}/grid/coordinates.skagbdy.nc .
 ln -s ${INPUT}/grid/domain_cfg_co9amm7_MEsL51r10-07.nc .

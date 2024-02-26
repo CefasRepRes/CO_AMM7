c74bae2e892fed72e304efd7d24cc0de081816ca
jenniferagraham <jennifer.graham@cefas.co.uk>
2024-02-26 10:25:01 +0000

    First commit with ERA5 and initial conditions for 1993

--- EXP4.0.4-parcels/setup-paths.sh
+++ EXP4.0.4-parcels/setup-paths.sh
@@ -2,6 +2,7 @@
 
 INPUT=/gpfs/data/cefas-shared/NEMO/jmmp/AMM7
 BDY_INPUT=/gpfs/data/cefas-shared/AMM7_reanalysis_V5/INPUTS
+RESTART=/gpfs/data/cefas-shared/AMM7_reanalysis_V5/RESTARTS/
 
 ln -s ${BDY_INPUT}/LBC/GLOSEA BDY
 ln -s ${BDY_INPUT}/LBC/BAL BDY_SKAG
@@ -10,8 +11,8 @@ ln -s ${BDY_INPUT}/SBC .
 ln -s ${INPUT}/grid/coordinates.bdy.nc .
 ln -s ${INPUT}/grid/coordinates.skagbdy.nc .
 ln -s ${INPUT}/grid/domain_cfg_co9amm7_MEsL51r10-07.nc .
-ln -s ${INPUT}/inputs/IC/initcd_y2005m01.nc .
 ln -s ${INPUT}/inputs/TIDE .
 
 mkdir RESTARTS
+ln -s ${RESTART}/restart_19930101.nc RESTARTS
 
